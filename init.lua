c6ec51382d86e59ce4e5f08b067d8aa039df594e
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2025-07-12 20:48:42 +0200

    feat: Bootstrap the lazy package manger

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
+--- Make sure to setup `mapleader` and `maplocalleader` before
+--- loading lazy.nvim so that mappings are correct.
 require("config.base")
 require("config.keymaps")
+
+require("config.lazy")
